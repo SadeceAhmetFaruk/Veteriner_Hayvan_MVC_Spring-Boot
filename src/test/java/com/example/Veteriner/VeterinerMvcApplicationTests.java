ff6e47ac5df35a9a55b234588b186630676615c6
SadeceAhmetFaruk <cybersteinmicro@gmail.com>
2022-08-13 00:33:46 +0300

    first commit

---
+++ src/test/java/com/example/Veteriner/VeterinerMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Veteriner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeterinerMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
